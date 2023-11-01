637c630e4ab60dd059260b09244217c863ee1e95
naomo123 <naomoiglesias@gmail.com>
2023-11-01 04:13:50 -0600

    FIxing commands, improving frontend Co-authored-by: Rodrigo Grande <rodrigogz64@users.noreply.github.com>

--- src/components/CLN.jsx
+++ src/components/CLN.jsx
@@ -6,172 +6,290 @@ import stepsjson from '../assets/content.json';
 import WeareWorking from './WeareWorking';
 
 const CLN = () => {
-    const [input, setInput] = useState('');
-    const [output, setOutput] = useState([]);
-    const [currentStep, setCurrentStep] = useState(0);
-    const terminalRef = useRef(null);
-    const [commandHistory, setCommandHistory] = useState([]);
-    const [isTypingResponse, setIsTypingResponse] = useState(false);
-    const predefinedResponses = presponses;
-    const steps = Object.values(stepsjson.steps);
-    const [isChecked, setIsChecked] = useState(true);
+  const [input, setInput] = useState('');
+  const [output, setOutput] = useState([]);
+  const [currentStep, setCurrentStep] = useState(0);
+  const terminalRef = useRef(null);
+  const [commandHistory, setCommandHistory] = useState([]);
+  const [isTypingResponse, setIsTypingResponse] = useState(false);
+  const predefinedResponses = presponses;
+  const steps = Object.values(stepsjson.steps);
+  const [isChecked, setIsChecked] = useState(true);
 
-    const handleCheckboxChange = (event) => {
-        setIsChecked(event.target.checked);
-        { console.log(isChecked) }
-    };
+  const handleCheckboxChange = (event) => {
+    setIsChecked(event.target.checked);
+    { console.log(isChecked) }
+  };
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const handleInput = () => {
-        if (isTypingResponse) {
-            return;
-        }
+  const handleInput = () => {
+    if (isTypingResponse) {
+      return;
+    }
 
-        const trimmedInput = input.trim();
-        console.log(trimmedInput)
-        if (trimmedInput === 'clear') {
-            setOutput([]);
-            setInput('');
-            setCommandHistory([]);
+    const trimmedInput = input.trim();
+    console.log(trimmedInput)
+    if (trimmedInput === 'clear') {
+      setOutput([]);
+      setInput('');
+      setCommandHistory([]);
 
-        } else if (trimmedInput) {
-            const newCommandHistory = [...commandHistory, trimmedInput];
+    } 
+    
+    else if (trimmedInput === 'l1-cli listchannels') {
+          
+              const responseArray = predefinedResponses[trimmedInput];
+            const responseIndex = commandHistory.filter((cmd) => cmd === 'l1-cli listchannels').length;
 
-            if (predefinedResponses.hasOwnProperty(trimmedInput)) {
-                const response = predefinedResponses[trimmedInput];
-                console.log(response);
+            if (responseArray && responseArray.length > 0) {
                 const newOutput = [...output, `root@linux:/ ${trimmedInput}`];
                 setOutput(newOutput);
 
-                const responseOutput = `root@linux:/ ${response}`;
-                newOutput.push(responseOutput);
-                setOutput([...newOutput]);
+                if (responseIndex < responseArray.length) {
+                    newOutput.push(`root@linux:/ ${responseArray[responseIndex]}`);
+                    setOutput([...newOutput]);
+                }
 
-                setIsTypingResponse(true);
-
-                let typingIndex = 0;
-                const typingInterval = setInterval(() => {
-                    if (typingIndex < response.length) {
-                        newOutput[newOutput.length - 1] = `${response.substring(0, typingIndex + 1)}`;
-                        setOutput([...newOutput]);
-                        typingIndex++;
-                    } else {
-                        clearInterval(typingInterval);
-                        setIsTypingResponse(false);
-                    }
-                },10);
+                const newCommandHistory = [...commandHistory, trimmedInput];
+                setCommandHistory(newCommandHistory);
+                setInput('');
             } else {
-                const newOutput = [...output, `root@linux:/ ${trimmedInput}`, <span className="error"> Comando no reconocido: ${trimmedInput} </span>];
+                const newOutput = [...output, `root@linux:/ ${trimmedInput}`, <span className="error">Comando no reconocido: ${trimmedInput}</span>];
                 setOutput(newOutput);
+                setInput('');
             }
-
-            setCommandHistory(newCommandHistory);
-            setInput('');
         }
-    };
+         else if (trimmedInput === 'l2-cli listchannels') {
+          
+              const responseArray = predefinedResponses[trimmedInput];
+            const responseIndex = commandHistory.filter((cmd) => cmd === 'l2-cli listchannels').length;
 
-    const handleNextStep = () => {
-        if (currentStep < steps.length - 1) {
-            setCurrentStep(currentStep + 1);
-        } else {
-            navigate('/achievementbox');
+            if (responseArray && responseArray.length > 0) {
+                const newOutput = [...output, `root@linux:/ ${trimmedInput}`];
+                setOutput(newOutput);
+
+                if (responseIndex < responseArray.length) {
+                    newOutput.push(`root@linux:/ ${responseArray[responseIndex]}`);
+                    setOutput([...newOutput]);
+                }
+
+                const newCommandHistory = [...commandHistory, trimmedInput];
+                setCommandHistory(newCommandHistory);
+                setInput('');
+            } else {
+                const newOutput = [...output, `root@linux:/ ${trimmedInput}`, <span className="error">Comando no reconocido: ${trimmedInput}</span>];
+                setOutput(newOutput);
+                setInput('');
+            }
         }
-    };
+        else if (trimmedInput === 'l2-cli getinfo') {
+          
+              const responseArray = predefinedResponses[trimmedInput];
+            const responseIndex = commandHistory.filter((cmd) => cmd === 'l2-cli getinfo').length;
 
-    useEffect(() => {
-        if (terminalRef.current) {
-            terminalRef.current.scrollTop = terminalRef.current.scrollHeight;
+            if (responseArray && responseArray.length > 0) {
+                const newOutput = [...output, `root@linux:/ ${trimmedInput}`];
+                setOutput(newOutput);
+
+                if (responseIndex < responseArray.length) {
+                    newOutput.push(`root@linux:/ ${responseArray[responseIndex]}`);
+                    setOutput([...newOutput]);
+                }
+
+                const newCommandHistory = [...commandHistory, trimmedInput];
+                setCommandHistory(newCommandHistory);
+                setInput('');
+            } else {
+                const newOutput = [...output, `root@linux:/ ${trimmedInput}`, <span className="error">Comando no reconocido: ${trimmedInput}</span>];
+                setOutput(newOutput);
+                setInput('');
+            }
         }
-    }, [output]);
 
-    return (
-        <div className="container">
+            else if (trimmedInput === 'l1-cli listfunds') {
+          
+              const responseArray = predefinedResponses[trimmedInput];
+            const responseIndex = commandHistory.filter((cmd) => cmd === 'l1-cli listfunds').length;
 
-            <div class="tabs">
-                <input type="radio" id="tab1" name="tab-control" checked={isChecked} onChange={handleCheckboxChange} />
-                <input type="radio" id="tab2" name="tab-control" />
-                <input type="radio" id="tab3" name="tab-control" />
-                <input type="radio" id="tab4" name="tab-control" />
+            if (responseArray && responseArray.length > 0) {
+                const newOutput = [...output, `root@linux:/ ${trimmedInput}`];
+                setOutput(newOutput);
 
-                <ul>
-                    <li title="Regtest"><label htmlFor="tab1" role="button"><span>Regtest</span></label></li>
-                    <li title="Tesnet"><label htmlFor="tab2" role="button"><span>Testnet</span></label></li>
-                    <li title="Signet"><label htmlFor="tab3" role="button"><span>Signet</span></label></li>
-                    <li title="Mainnet"><label htmlFor="tab4" role="button"><span>Mainnet</span></label></li>
-                </ul>
-                <div className="slider"><div className="indicator"></div></div>
-                <div className="content">
-                    <section className='sectionReg'>
+                if (responseIndex < responseArray.length) {
+                    newOutput.push(`root@linux:/ ${responseArray[responseIndex]}`);
+                    setOutput([...newOutput]);
+                }
 
-                        <div className="steps">
-                            <div className="step">{steps[currentStep]}</div>
+                const newCommandHistory = [...commandHistory, trimmedInput];
+                setCommandHistory(newCommandHistory);
+                setInput('');
+            } else {
+                const newOutput = [...output, `root@linux:/ ${trimmedInput}`, <span className="error">Comando no reconocido: ${trimmedInput}</span>];
+                setOutput(newOutput);
+                setInput('');
+            }
+        }
+        else if (trimmedInput === 'l2-cli listfunds') {
+          
+              const responseArray = predefinedResponses[trimmedInput];
+            const responseIndex = commandHistory.filter((cmd) => cmd === 'l2-cli listfunds').length;
 
-                            <button onClick={handleNextStep} className="next-button">
-                                {currentStep === steps.length - 1 ? 'Terminar' : 'Siguiente'}
-                            </button>
-                        </div>
+            if (responseArray && responseArray.length > 0) {
+                const newOutput = [...output, `root@linux:/ ${trimmedInput}`];
+                setOutput(newOutput);
 
-                        <div className="terminal" ref={terminalRef}>
-                            {output.map((line, index) => {
-                                const promptLength = 'root@linux:/'.length;
-                                const isResponse = index % 2 === 1;
-                                const lines = typeof line === 'string' ? line.split('\n') : [line];
+                if (responseIndex < responseArray.length) {
+                    newOutput.push(`root@linux:/ ${responseArray[responseIndex]}`);
+                    setOutput([...newOutput]);
+                }
 
+                const newCommandHistory = [...commandHistory, trimmedInput];
+                setCommandHistory(newCommandHistory);
+                setInput('');
+            } else {
+                const newOutput = [...output, `root@linux:/ ${trimmedInput}`, <span className="error">Comando no reconocido: ${trimmedInput}</span>];
+                setOutput(newOutput);
+                setInput('');
+            }
+        }
+        else if (trimmedInput) {
+      const newCommandHistory = [...commandHistory, trimmedInput];
 
-                                return (
-                                    <div key={index}>
-                                        {lines.map((text, lineIndex) => (
-                                            <div key={lineIndex} className={`output-line ${isResponse ? 'response-line' : ''}`}>
-                                                {typeof text === 'string' && text.startsWith('root@linux:/') ? (
-                                                    <>
-                                                        <span className="prompt">
-                                                            {text.substring(0, promptLength)}
-                                                        </span>
-                                                        {text.substring(promptLength)}
-                                                    </>
-                                                ) : (
-                                                    text
-                                                )}
-                                            </div>
-                                        ))}
-                                    </div>
-                                );
-                            })}
+      if (predefinedResponses.hasOwnProperty(trimmedInput)) {
+        const response = predefinedResponses[trimmedInput];
+        console.log(response);
+        const newOutput = [...output, `root@linux:/ ${trimmedInput}`];
+        setOutput(newOutput);
 
-                            <div className="input-line-container">
-                                <span className="prompt">root@linux:/</span>
-                                <input
-                                    className="input-line"
-                                    type="text"
-                                    value={input}
-                                    onChange={(event) => setInput(event.target.value)}
-                                    onKeyDown={(event) => {
-                                        if (event.key === 'Enter') {
-                                            handleInput();
-                                        }
-                                    }}
-                                />
-                            </div>
-                        </div>
-                    </section>
-                    <section className='sectionTest'>
-                    <WeareWorking/>
-                        </section>
-                    <section className='sectionSig'>
-                    <WeareWorking/>
-                        </section>
-                    <section className='sectionMain'>
-                           <WeareWorking/>
-                        </section>
-                </div>
+        const responseOutput = `root@linux:/ ${response}`;
+        newOutput.push(responseOutput);
+        setOutput([...newOutput]);
+
+        setIsTypingResponse(true);
+
+        let typingIndex = 0;
+        const typingInterval = setInterval(() => {
+          if (typingIndex < response.length) {
+            newOutput[newOutput.length - 1] = `${response.substring(0, typingIndex + 1)}`;
+            setOutput([...newOutput]);
+            typingIndex++;
+          } else {
+            clearInterval(typingInterval);
+            setIsTypingResponse(false);
+          }
+        }, 10);
+      } else {
+        const newOutput = [...output, `root@linux:/ ${trimmedInput}`, <span className="error"> Comando no reconocido: ${trimmedInput} </span>];
+        setOutput(newOutput);
+      }
+
+      setCommandHistory(newCommandHistory);
+      setInput('');
+    }
+  };
+
+  const handleNextStep = () => {
+    if (currentStep < steps.length - 1) {
+      setCurrentStep(currentStep + 1);
+    } else {
+      navigate('/achievementbox');
+    }
+  };
+
+  useEffect(() => {
+    if (terminalRef.current) {
+      terminalRef.current.scrollTop = terminalRef.current.scrollHeight;
+    }
+  }, [output]);
+
+  return (
+    <div className="container">
+
+      <div class="tabs">
+        <input type="radio" id="tab1" name="tab-control" checked={isChecked} onChange={handleCheckboxChange} />
+        <input type="radio" id="tab2" name="tab-control" />
+        <input type="radio" id="tab3" name="tab-control" />
+        <input type="radio" id="tab4" name="tab-control" />
+
+        <ul>
+          <li title="Regtest"><label htmlFor="tab1" role="button"><span>Regtest</span></label></li>
+          <li title="Tesnet"><label htmlFor="tab2" role="button"><span>Testnet</span></label></li>
+          <li title="Signet"><label htmlFor="tab3" role="button"><span>Signet</span></label></li>
+          <li title="Mainnet"><label htmlFor="tab4" role="button"><span>Mainnet</span></label></li>
+        </ul>
+        <div className="slider"><div className="indicator"></div></div>
+        <div className="content">
+          <section className='sectionReg'>
+
+            <div className="steps">
+              <div className="step">{steps[currentStep]}</div>
+
+              <button onClick={handleNextStep} className="next-button">
+                {currentStep === steps.length - 1 ? 'Terminar' : 'Siguiente'}
+              </button>
             </div>
 
+            <div className="terminal" ref={terminalRef}>
+              {output.map((line, index) => {
+                const promptLength = 'root@linux:/'.length;
+                const isResponse = index % 2 === 1;
+                const lines = typeof line === 'string' ? line.split('\n') : [line];
 
 
+                return (
+                  <div key={index}>
+                    {lines.map((text, lineIndex) => (
+                      <div key={lineIndex} className={`output-line ${isResponse ? 'response-line' : ''}`}>
+                        {typeof text === 'string' && text.startsWith('root@linux:/') ? (
+                          <>
+                            <span className="prompt">
+                              {text.substring(0, promptLength)}
+                            </span>
+                            {text.substring(promptLength)}
+                          </>
+                        ) : (
+                          text
+                        )}
+                      </div>
+                    ))}
+                  </div>
+                );
+              })}
 
+              <div className="input-line-container">
+                <span className="prompt">root@linux:/</span>
+                <input
+                  className="input-line"
+                  type="text"
+                  value={input}
+                  onChange={(event) => setInput(event.target.value)}
+                  onKeyDown={(event) => {
+                    if (event.key === 'Enter') {
+                      handleInput();
+                    }
+                  }}
+                />
+              </div>
+            </div>
+          </section>
+          <section className='sectionTest'>
+            <WeareWorking />
+          </section>
+          <section className='sectionSig'>
+            <WeareWorking />
+          </section>
+          <section className='sectionMain'>
+            <WeareWorking />
+          </section>
         </div>
-    );
+      </div>
+
+
+
+
+    </div>
+  );
 };
 
 export default CLN;
--- src/components/HomeScreen.jsx
+++ src/components/HomeScreen.jsx
@@ -1,95 +1,95 @@
-import React, { useState } from 'react';
-import '../styles/HomeScreen.css';
-import content from '../assets/content.json';
+  import React, { useState } from 'react';
+  import '../styles/HomeScreen.css';
+  import content from '../assets/content.json';
 
-import logo from '../assets/logotest.svg';
-import icono1 from '../assets/icono1.svg';
-import icono2 from '../assets/icon2.svg';
-import icono3 from '../assets/icono3.svg';
-import icono4 from '../assets/icono4.svg';
-import icono5 from '../assets/icono5.svg';
-import rsc1 from '../assets/whitepaper.png';
-import rsclinux from '../assets/linux.webp'
+  import logo from '../assets/logotest.svg';
+  import icono1 from '../assets/icono1.svg';
+  import icono2 from '../assets/icon2.svg';
+  import icono3 from '../assets/icono3.svg';
+  import icono4 from '../assets/icono4.svg';
+  import icono5 from '../assets/icono5.svg';
+  import rsc1 from '../assets/whitepaper.png';
+  import rsclinux from '../assets/linux.webp'
 
-const HomeScreen = () => {
-  const [buttonVisible, setButtonVisible] = useState(true);
-  const [expanded, setExpanded] = useState(false);
-  const generalContent = content.generalContent;
+  const HomeScreen = () => {
+    const [buttonVisible, setButtonVisible] = useState(true);
+    const [expanded, setExpanded] = useState(false);
+    const generalContent = content.generalContent;
 
-  const handleStartClick = () => {
-    setButtonVisible(false);
-    setExpanded(true);
-  };
+    const handleStartClick = () => {
+      setButtonVisible(false);
+      setExpanded(true);
+    };
 
-  return (
-    <main className="main">
-      <div className={`container-i ${expanded ? 'expanded' : ''}`}>
-        <div className='rocket'>
-          <img src={logo} alt="ZapZone Learning Logo" className="logo" />
+    return (
+      <main className="main">
+        <div className={`container-i ${expanded ? 'expanded' : ''}`}>
+          <div className='rocket'>
+            <img src={logo} alt="ZapZone Learning Logo" className="logo" />
 
 
-        </div>
-        <div className={`start-button ${buttonVisible ? '' : 'hide'}`}>
-          <button onClick={handleStartClick}>{generalContent['btn-start']}</button>
-        </div>
-        {expanded && (
+          </div>
+          <div className={`start-button ${buttonVisible ? '' : 'hide'}`}>
+            <button onClick={handleStartClick}>{generalContent['btn-start']}</button>
+          </div>
+          {expanded && (
 
-          <>
-            <div className="expanded-content">
-              <h1 className="title">Key Features</h1>
-              <div className='section2'>
-                <ul className="list">
-                  {content.list.map((list, index) => (
-                    <li className="list__item" key={index}>
-                      <h1 className="titlesect2">{list.title}</h1>
-                      <p className="txtsect2">{list.content}</p>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-              <h1 className="title">Fostering Dynamic Learning: Our Platform's Key Aspects</h1>
-              <div className="section1">
-
-                <div className='cards'>
-                  {content.cards.map((card, index) => (
-                    <div className="card" key={index}>
-                      <div className="card-logo">
-                        {card.image && (
-                          <img className="img-card" src={index === 0 ? icono1 : index === 1 ? icono2 : index === 2 ? icono3 : index === 3 ? icono4 : icono5} alt={`Card ${index}`} />
-                        )}
+            <>
+              <div className="expanded-content">
+                <h1 className="title">Key Features</h1>
+                <div className='section2'>
+                  <div className="list">
+                    {content.list.map((list, index) => (
+                      <div className="list__item" key={index}>
+                        <h1 className="titlesect2">{list.title}</h1>
+                        <p className="txtsect2">{list.content}</p>
                       </div>
-                      <h4 className="titlecards">{card.title}</h4>
-                      <p className="txtcards">{card.content}</p>
-                    </div>
-                  ))}
+                    ))}
+                  </div>
+                </div>
+                <h1 className="title">Fostering Dynamic Learning: Our Platform's Key Aspects</h1>
+                <div className="section1">
+
+                  <div className='cards'>
+                    {content.cards.map((card, index) => (
+                      <div className="card" key={index}>
+                        <div className="card-logo">
+                          {card.image && (
+                            <img className="img-card" src={index === 0 ? icono1 : index === 1 ? icono2 : index === 2 ? icono3 : index === 3 ? icono4 : icono5} alt={`Card ${index}`} />
+                          )}
+                        </div>
+                        <h4 className="titlecards">{card.title}</h4>
+                        <p className="txtcards">{card.content}</p>
+                      </div>
+                    ))}
+                  </div>
+
+
                 </div>
 
+                <div className="section3">
+                  <div className='text-row'> <h1 className="title">
+                    Useful Links
+                  </h1>
+                  </div>
+                  <div className='list2'>
+                    {content.resources.map((resource, index) => (
+                      <a key={index} href={resource.content} className="img-card-pre">
+                        <img className='card-img-rsc' src={index === 0 ? rsc1 : rsclinux} alt={`Resource ${index}`} />
+                        <h1 className="img-card-pre__title">
+                          {resource.title}
+                        </h1>
+                      </a>
+                    ))}
+                  </div>
 
+                </div>
               </div>
+            </>
+          )}
+        </div>
+      </main>
+    );
+  }
 
-              <div className="section3">
-                <div className='text-row'> <h1 className="title">
-                  Useful Links
-                </h1>
-                </div>
-                <div className='list2'>
-                  {content.resources.map((resource, index) => (
-                    <a key={index} href={resource.content} className="img-card-pre">
-                      <img className='card-img-rsc' src={index === 0 ? rsc1 : rsclinux} alt={`Resource ${index}`} />
-                      <h1 className="img-card-pre__title">
-                        {resource.title}
-                      </h1>
-                    </a>
-                  ))}
-                </div>
-
-              </div>
-            </div>
-          </>
-        )}
-      </div>
-    </main>
-  );
-}
-
-export default HomeScreen;
+  export default HomeScreen;
